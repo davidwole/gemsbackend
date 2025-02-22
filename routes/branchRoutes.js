f340a2aa8640dac19a3d279db6db775acf89b3bc
davidwole <davidwole20@gmail.com>
2025-02-22 12:37:52 +0100

    Added L2 Branch Get Access

--- routes/branchRoutes.js
+++ routes/branchRoutes.js
@@ -9,7 +9,7 @@ const { authMiddleware, authorize } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.get("/", authMiddleware, authorize(["L1"]), getBranch);
+router.get("/", authMiddleware, authorize(["L1", "L2"]), getBranch);
 router.post("/", authMiddleware, authorize(["L1"]), createBranch);
 router.patch("/:id", authMiddleware, authorize(["L1"]), updateBranch);
 router.delete("/:id", authMiddleware, authorize(["L1"]), deleteBranch);
